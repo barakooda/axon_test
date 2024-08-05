959fec5c3fecd713c1a915e417fe4262cf1970d0
Barak Koren <barakood@gmail.com>
2024-08-05 11:29:53 +0300

    better naming

--- src/detector.py
+++ src/detector.py
@@ -20,10 +20,10 @@ def detector(frame_queue, result_queue):
         diff = cv2.absdiff(gray_frame, prev_frame)
         thresh = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)[1]
         thresh = cv2.dilate(thresh, None, iterations=2)
-        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
+        contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
 
-        filtered_cnts = [c for c in cnts if cv2.contourArea(c) >= 500]
+        filtered_contours = [contour for contour in contours if cv2.contourArea(contour) >= 500]
 
         prev_frame = gray_frame
-        result_queue.put({'frame': frame, 'contours': filtered_cnts})
+        result_queue.put({'frame': frame, 'contours': filtered_contours})
