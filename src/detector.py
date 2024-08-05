53a66c270b63144fa4159fa8f72e6d3c8b4ba731
Barak Koren <barakood@gmail.com>
2024-08-05 11:42:10 +0300

    Assume we use opencv 4 and up,we can ommit grab_contours and imutils.

--- src/detector.py
+++ src/detector.py
@@ -1,5 +1,4 @@
 import cv2
-import imutils
 
 def detector(frame_queue, result_queue):
     prev_frame = None
@@ -20,9 +19,10 @@ def detector(frame_queue, result_queue):
         diff = cv2.absdiff(gray_frame, prev_frame)
         thresh = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)[1]
         thresh = cv2.dilate(thresh, None, iterations=2)
-        contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contours = imutils.grab_contours(contours)
 
+        contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Filter contours based on area size
         filtered_contours = [contour for contour in contours if cv2.contourArea(contour) >= 500]
 
         prev_frame = gray_frame
